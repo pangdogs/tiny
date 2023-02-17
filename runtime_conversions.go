2f6e7cf233db4deb963a6db3fc2fa6000c6b01b0
pangdogs <pangdogs@gmail.com>
2023-02-18 02:54:12 +0800

    整理代码

--- runtime_conversions.go
+++ runtime_conversions.go
@@ -2,7 +2,7 @@ package tiny
 
 import "kit.golaxy.org/tiny/util"
 
-// GetRuntimeInheritor 获取运行时的继承者
-func GetRuntimeInheritor[T any](runtime Runtime) T {
+// GetInheritor 获取运行时的继承者
+func GetInheritor[T any](runtime Runtime) T {
 	return util.Cache2Iface[T](runtime.getOptions().Inheritor.Cache)
 }
