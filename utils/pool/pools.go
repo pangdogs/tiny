9160b135472fa3f87d55244ba2fdf3fb52fa54c7
pangdogs <pangdogs@gmail.com>
2024-06-27 23:53:09 +0800

    修改代码

--- utils/pool/pools.go
+++ utils/pool/pools.go
@@ -1,36 +1,55 @@
 package pool
 
 import (
+	"fmt"
 	"git.golaxy.org/tiny/utils/generic"
 )
 
-var pools = map[string]*Pool{}
+var (
+	poolNames = map[string]*Pool{}
+	poolIds   = map[uint32]*Pool{}
+)
 
 func Declare[T any](chunkSize int64) *Pool {
 	pool := NewPool[T](chunkSize)
 
-	existed, ok := pools[pool.Name()]
+	existed, ok := poolNames[pool.Name()]
 	if ok {
 		if existed.chunkSize < chunkSize {
 			existed.chunkSize = chunkSize
 		}
 		return existed
 	}
-	pools[pool.Name()] = pool
+
+	existed, ok = poolIds[pool.Id()]
+	if ok {
+		panic(fmt.Errorf("pool(%d) has already been declared by %q", pool.Id(), existed.Name()))
+	}
+
+	poolNames[pool.Name()] = pool
+	poolIds[pool.Id()] = pool
 
 	return pool
 }
 
 func Get[T any](name string) *Chunk[T] {
-	return pools[name].Get().(*Chunk[T])
+	return poolNames[name].Get().(*Chunk[T])
+}
+
+func GetById[T any](id uint32) *Chunk[T] {
+	return poolIds[id].Get().(*Chunk[T])
 }
 
 func Put[T any](name string, chunk *Chunk[T]) {
-	pools[name].Put(chunk)
+	poolNames[name].Put(chunk)
+}
+
+func PutById[T any](id uint32, chunk *Chunk[T]) {
+	poolIds[id].Put(chunk)
 }
 
 func Range(fun generic.Func2[string, *Pool, bool]) {
-	for name, pool := range pools {
+	for name, pool := range poolNames {
 		if !fun.Exec(name, pool) {
 			return
 		}
@@ -38,7 +57,7 @@ func Range(fun generic.Func2[string, *Pool, bool]) {
 }
 
 func Each(fun generic.Action2[string, *Pool]) {
-	for name, pool := range pools {
+	for name, pool := range poolNames {
 		fun.Exec(name, pool)
 	}
 }
@@ -54,7 +73,7 @@ type Info struct {
 func Stat() []Info {
 	infos := generic.MakeSliceMap[string, Info]()
 
-	for name, pool := range pools {
+	for name, pool := range poolNames {
 		infos.Add(name, Info{
 			Name:       pool.Name(),
 			ChunkSize:  pool.ChunkSize(),
