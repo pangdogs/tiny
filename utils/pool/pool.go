ca5c3f6f2208b18eb7a03b67755be98e1972c946
pangdogs <pangdogs@gmail.com>
2024-06-27 15:06:13 +0800

    修改代码

--- utils/pool/pool.go
+++ utils/pool/pool.go
@@ -1,57 +1,29 @@
 package pool
 
 import (
-	"context"
 	"git.golaxy.org/tiny/utils/types"
-	common_pool "github.com/jolestar/go-commons-pool/v2"
+	"sync"
 	"sync/atomic"
 )
 
 func NewPool[T any]() *Pool {
 	pool := &Pool{}
 	pool.name = types.FullNameT[T]()
-
-	pool.pool = common_pool.NewObjectPool(
-		context.Background(),
-		common_pool.NewPooledObjectFactory(
-			func(ctx context.Context) (interface{}, error) {
-				atomic.AddInt64(&pool.allocNum, 1)
-				return types.NewT[T](), nil
-			},
-			nil,
-			nil,
-			nil,
-			func(ctx context.Context, obj *common_pool.PooledObject) error {
-				*(obj.Object.(*T)) = types.ZeroT[T]()
-				atomic.AddInt64(&pool.putNum, 1)
-				return nil
-			},
-		),
-		&common_pool.ObjectPoolConfig{
-			LIFO:                     false,
-			MaxTotal:                 -1,
-			MaxIdle:                  common_pool.DefaultMaxIdle,
-			MinIdle:                  common_pool.DefaultMinIdle,
-			TestOnCreate:             false,
-			TestOnBorrow:             false,
-			TestOnReturn:             false,
-			TestWhileIdle:            false,
-			BlockWhenExhausted:       common_pool.DefaultBlockWhenExhausted,
-			MinEvictableIdleTime:     common_pool.DefaultMinEvictableIdleTime,
-			SoftMinEvictableIdleTime: common_pool.DefaultSoftMinEvictableIdleTime,
-			NumTestsPerEvictionRun:   common_pool.DefaultNumTestsPerEvictionRun,
-			EvictionPolicyName:       common_pool.DefaultEvictionPolicyName,
-			TimeBetweenEvictionRuns:  common_pool.DefaultTimeBetweenEvictionRuns,
-			EvictionContext:          context.Background(),
-		},
-	)
-
+	pool.pool.New = func() any {
+		atomic.AddInt64(&pool.allocNum, 1)
+		return types.NewT[T]()
+	}
+	pool.zero = func(obj any) any {
+		*(obj.(*T)) = types.ZeroT[T]()
+		return obj
+	}
 	return pool
 }
 
 type Pool struct {
 	name                     string
-	pool                     *common_pool.ObjectPool
+	pool                     sync.Pool
+	zero                     func(obj any) any
 	allocNum, getNum, putNum int64
 }
 
@@ -59,18 +31,13 @@ func (p *Pool) Name() string {
 	return p.name
 }
 
-func (p *Pool) Prepare(num int64) {
-	p.pool.Config.MinIdle = int(num)
-	p.pool.Config.MaxIdle = int(num)
-	p.pool.PreparePool(context.Background())
-}
-
 func (p *Pool) Put(obj any) {
-	p.pool.ReturnObject(context.Background(), obj)
+	p.pool.Put(p.zero(obj))
+	atomic.AddInt64(&p.putNum, 1)
 }
 
 func (p *Pool) Get() any {
-	v, _ := p.pool.BorrowObject(context.Background())
+	v := p.pool.Get()
 	atomic.AddInt64(&p.getNum, 1)
 	return v
 }
